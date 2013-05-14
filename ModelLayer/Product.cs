e0e6d6f0ada6a0b76a13a433e3565b0ec92efb28
Christian Nørgaard <Chrenner10@hotmail.com>
2013-05-14 12:42:26 +0200

    Created Product class in Modellayer

---
+++ ModelLayer/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ModelLayer
+{
+    class Product
+    {
+        //Values
+        public string productName { get; set; }
+
+        public Product(string productname)
+        {
+            this.productName = productName;
+        }
+    }
+}
