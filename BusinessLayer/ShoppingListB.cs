2aca3e8499a00f4897340c3a6f2a13f949a0c752
Kasper Bager Mortensen <kasparu4@gmail.com>
2013-05-14 11:32:06 +0200

    Trying to commit again

--- BusinessLayer/ShoppingListB.cs
+++ BusinessLayer/ShoppingListB.cs
@@ -1,22 +1,25 @@
-﻿using System;
+﻿using ModelLayer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace BusinessLayer
 {
-    public class ShoppingListB
+    public class ShoppingListB : IFShoppingListB
     {
 
         public ShoppingListB()
         {
         }
 
-        public void createShoppingList(string listName, long completionDate, User user)
+        public void createShoppingList(string shoppingListName, long completionDate, int userId)
         {
             TimeSpan timeSpan = DateTime.UtcNow - new DateTime(1970,1,1,0,0,0);
 
-            ShoppingList shoppingList = new ShoppingList(string listName, (long)timeSpan.TotalSeconds, completionDate);
+            ShoppingList shoppingList = new ShoppingList(shoppingListName, (long)timeSpan.TotalSeconds, completionDate);
+            
+
         }
     }
 
--- ModelLayer/ShoppingList.cs
+++ ModelLayer/ShoppingList.cs
@@ -8,20 +8,22 @@ namespace ModelLayer
     public class ShoppingList
     {
         //Values
-        public string listName { get; set; }
+        public string shoppingListName { get; set; }
         public long creationDate { get; set; }
         public long completionDate { get; set; }
 
         //Lists
-        //public List<User> users { get; set; }
+        public List<User> users { get; set; }
         //public List<Product> products { get; set; }
 
 
-        public ShoppingList(string listName, long creationDate, long completionDate)
+        public ShoppingList(string shoppingListName, long creationDate, long completionDate)
         {
-            this.listName = listName;
+            this.shoppingListName = shoppingListName;
             this.creationDate = creationDate;
             this.completionDate = completionDate;
+
+            this.users = new List<User>();
         }
     }
 }
