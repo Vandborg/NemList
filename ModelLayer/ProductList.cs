6d9c573a1cb7089d5511fa83579fe2307546bfd8
Kasper Bager Mortensen <kasparu4@gmail.com>
2013-05-14 09:18:52 +0200

    Have made a TestLayer for our unit tests and other.

---
+++ ModelLayer/ProductList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ModelLayer
+{
+    public class ProductList
+    {
+        //Values
+        public string listName { get; set; }
+        public DateTime creationDate { get; set; }
+        public DateTime completionDate { get; set; }
+
+        //Lists
+        public List<User> users { get; set; }
+        public List<Product> products { get; set; }
+
+
+        public ProductList()
+        {
+
+        }
+    }
+}
