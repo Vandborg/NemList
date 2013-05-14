69c4ac08e9248b65ab29c913ebedad7e0cd21b28
Christian Bjerg <cbjerg@gmail.com>
2013-05-14 12:09:51 +0200

    WCF updated

--- ModelLayer/User.cs
+++ ModelLayer/User.cs
@@ -1,16 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace ModelLayer
 {
+    [DataContract]
     public class User
     {
+        [DataMember]
         private int _userId;
+        [DataMember]
         private string FName { get; set; }
+        [DataMember]
         private string LName { get; set; }
+        [DataMember]
         private string UserName { get; set; }
+        [DataMember]
         private string Email { get; set; }
 
         public User(string fname, string lname, string uname, string email)
---
+++ WCF/ListService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using BusinessLayer;
+
+namespace WCF
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class ListService : IListService
+    {
+        private static IFUserB userObj = new UserB();
+
+        public void CreateUser(string fname, string lname, string uname, string email)
+        {
+            userObj.CreateUser(fname, lname, uname, email);
+        }
+    }
+}
